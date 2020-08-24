f10ad2e159833748cc59533bea66d0f08058e573
klovae <elyse.klova@gmail.com>
2020-08-24 19:42:32 -0400

    new user form working with correct params

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
+  get '/login', to: 'sessions#new'
+
   get '/welcome', to: 'static#welcome'
   get '/about', to: 'static#about'
   root 'static#index'
-  
+
 end
