144ed107608b1d44683d7ad44f09e17bd9c1890d
klovae <elyse.klova@gmail.com>
2020-08-24 19:24:35 -0400

    Created view files and user new form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :time_zone)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create. :show]
+  resources :users, only: [:new, :create, :show]
+
+  get '/welcome', to: 'static#welcome'
+  get '/about', to: 'static#about'
+  root 'static#index'
+  
 end
