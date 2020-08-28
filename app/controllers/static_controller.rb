2b5aae4154ab2aba89bb617cbd0176315d42ac8c
klovae <elyse.klova@gmail.com>
2020-08-27 22:48:37 -0400

    started functionality to display permissions invites on login

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,10 @@ class StaticController < ApplicationController
   skip_before_action :auth_required, only: [:index, :about]
 
   def welcome
+    invites = current_user.permissions.where(accepted: false)
+    if invites.present?
+      @invitations = invites
+    end
   end
 
   def index
